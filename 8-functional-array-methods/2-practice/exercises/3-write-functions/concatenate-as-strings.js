967e99c88c0e751bd55251752b5bc44690e862c5
emrahhko <emrah.hac@gmail.com>
2024-04-15 16:41:22 +0200

    done some exercises

--- 8-functional-array-methods/2-practice/exercises/3-write-functions/concatenate-as-strings.js
+++ 8-functional-array-methods/2-practice/exercises/3-write-functions/concatenate-as-strings.js
@@ -4,6 +4,18 @@
  * casts all values to a string and concatenates
  * returns a new array without modifying the original
  * @param {Array} arr - the array of values to filter
- * @returns {Array} - a new array with no strings
+ * @returns {string} - a new array with no strings
  */
-export const concatenateAsStrings = (arr = []) => {};
+
+export const concatenateAsStrings = (arr = []) => {
+  return arr.reduce((acc, item) => acc + String(item), '');
+};
+
+
+// export const concatenateAsStrings = (arr = []) => {
+//   let result = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     result = result + arr[i];
+//   }
+//   return result;
+// };
